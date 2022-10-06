c8f9096310286e39440dec9535dba24e3c5ce684
Damian Campana <dcampana@getontop.com>
2022-10-06 10:07:11 -0300

    remove comments

--- src/main/java/com/javatodev/api/model/User.java
+++ src/main/java/com/javatodev/api/model/User.java
@@ -20,25 +20,9 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    //@Column(unique = true)
     private String userName;
     private String password;
     private Boolean active;
     private String roles;
 
-    //
-/*
-    public User(Long id, String username, String password) {
-        this.id = id;
-        this.username = username;
-        this.password = password;
-    }
-
-    @OneToMany(cascade = CascadeType.ALL)
-    List<UserAuthority> authorities = new ArrayList<>();
-
-    public void addAuthorities(String authority) {
-        this.authorities.add(new UserAuthority(null, this, authority));
-    }*/
 }
